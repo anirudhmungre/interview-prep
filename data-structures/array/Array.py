eda096fc183ebaa95b15b7c369daf564d134383e
Anirudh Mungre <anirudh.mungre16@gmail.com>
2020-01-28 17:49:08 -0500

    setup array

---
+++ data-structures/array/Array.py
@@ -0,0 +1,16 @@
+class Array(object):
+    def __init__(self):
+        raise NotImplementedError
+
+    def __len__(self):
+        raise NotImplementedError
+
+    def get(self, parameter_list):
+        raise NotImplementedError
+    
+    def insert(self, value):
+        raise NotImplementedError
+
+    def delete(self, index):
+        raise NotImplementedError
+    
