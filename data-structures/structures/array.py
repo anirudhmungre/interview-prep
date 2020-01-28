5437f158da2ef2550ba8399c8860364ee9b0a4cd
Anirudh Mungre <anirudh.mungre16@gmail.com>
2020-01-28 18:55:12 -0500

    Array data structure

---
+++ data-structures/structures/array.py
@@ -0,0 +1,33 @@
+class Array(object):
+    def __init__(self, size=0, data_type=str):
+        self._size = size
+        self._data = [None] * size
+        self.data_type = data_type
+
+    def __str__(self):
+        return str(self._data)
+
+    def __len__(self):
+        return self._size
+    
+    def __check_index(self, index):
+        if abs(index) > len(self)-1:
+            raise IndexError(f'Array of size {len(self)} has no index at {index}')
+        return True
+    
+    def __check_type(self, value):
+        if type(value) is not self.data_type:
+            raise TypeError(f'This array is of type: {self.data_type}')
+        return True
+
+    def get(self, index):
+        if self.__check_index(index):
+            return self._data[index]
+
+    def insert(self, index, value):
+        if self.__check_index(index) and self.__check_type(value):
+            self._data[index] = value
+
+    def delete(self, index):
+        if self.__check_index(index):
+            self._data[index] = None
---
+++ data-structures/test.py
@@ -0,0 +1,23 @@
+from structures.array import Array
+
+# Testing the Array data structure
+arr = Array(5, int)
+
+try:
+    arr.insert(1, "hi")
+except TypeError as err:
+    print(err)
+try:
+    arr.get(10)
+except IndexError as err:
+    print(err)
+
+print(len(arr))
+print(arr)
+
+arr.insert(2, 2)
+print(arr)
+
+arr.delete(2)
+print(arr)
+# Done testing the Array data structure
