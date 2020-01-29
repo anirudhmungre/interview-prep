d519466eade4ff849ef17aef1abeb34e6239554c
Anirudh Mungre <anirudh.mungre16@gmail.com>
2020-01-29 13:57:47 -0500

    fix return typing for py3

--- data-structures/structures/array.py
+++ data-structures/structures/array.py
@@ -4,34 +4,34 @@ class Array(object):
         self._data = [None] * size
         self.data_type = data_type
 
-    def __str__(self):
+    def __str__(self) -> str:
         return str(self._data)
 
-    def __len__(self):
+    def __len__(self) -> int:
         return self._size
 
     def __iter__(self):
         for value in self._data:
             yield value
     
-    def __check_index(self, index):
+    def __check_index(self, index) -> bool:
         if abs(index) > len(self)-1:
             raise IndexError(f'Array of size {len(self)} has no index at {index}')
         return True
     
-    def __check_type(self, value):
+    def __check_type(self, value) -> bool:
         if type(value) is not self.data_type:
-            raise TypeError(f'This array is of type: {self.data_type}')
+            raise TypeError(f'Cannot use {type(value)} in array of type: {self.data_type}')
         return True
 
     def get(self, index):
         if self.__check_index(index):
             return self._data[index]
 
-    def insert(self, index, value):
+    def insert(self, index, value) -> None:
         if self.__check_index(index) and self.__check_type(value):
             self._data[index] = value
 
-    def delete(self, index):
+    def delete(self, index) -> None:
         if self.__check_index(index):
             self._data[index] = None
--- data-structures/structures/stack.py
+++ data-structures/structures/stack.py
@@ -3,26 +3,26 @@ class Stack(object):
         self._data = list()
         self.data_type = data_type
 
-    def __str__(self):
+    def __str__(self) -> str:
         return str(self._data)
     
-    def __len__(self):
+    def __len__(self) -> int:
         return len(self._data)
     
-    def __check_type(self, value):
+    def __check_type(self, value) -> bool:
         if type(value) is not self.data_type:
             raise TypeError(f'Cannot use {type(value)} in stack of type: {self.data_type}')
         return True
     
-    def __check_has_data(self):
+    def __check_has_data(self) -> bool:
         if self.isEmpty():
             raise IndexError('Empty stack has no more data.')
         return True
 
-    def isEmpty(self):
+    def isEmpty(self) -> bool:
         return len(self._data) <= 0
 
-    def push(self, value):
+    def push(self, value) -> None:
         if self.__check_type(value):
             self._data.append(value)
     
