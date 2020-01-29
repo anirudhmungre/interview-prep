e0f2be96a30edd2370be9d7889e5d01d92460124
Anirudh Mungre <anirudh.mungre16@gmail.com>
2020-01-29 16:36:48 -0500

    queue finished

---
+++ data-structures/queue.py
@@ -0,0 +1,35 @@
+class Queue(object):
+    def __init__(self, data_type=str):
+        self._data = list()
+        self.data_type = data_type
+
+    def __str__(self) -> str:
+        return str(self._data)
+    
+    def __len__(self) -> int:
+        return len(self._data)
+
+    def _check_type(self, value) -> bool:
+        if type(value) is not self.data_type:
+            raise TypeError(f'Cannot use {type(value)} in Queue of type: {self.data_type}')
+        return True
+
+    def _check_has_data(self) -> bool:
+        if self.isEmpty():
+            raise IndexError('Empty Queue has no more data.')
+        return True
+    
+    def enqueue(self, value):
+        if self._check_type(value):
+            self._data.insert(0, value)
+
+    def dequeue(self):
+        if self._check_has_data():
+            return self._data.pop()
+
+    def isEmpty(self):
+        return len(self._data) <= 0
+
+    def top(self):
+        if self._check_has_data():
+            return self._data[-1]
